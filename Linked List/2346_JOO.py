4155ae55ec7ea291ee9e1e63917bf2269f6e3f97
Joo <sr7418@naver.com>
2021-09-25 02:04:40 +0900

    try with linked list

---
+++ Linked List/2346_JOO.py
@@ -0,0 +1,19 @@
+import sys
+n = int(sys.stdin.readline())
+
+ballon = list(map(int, sys.stdin.readline().split()))
+indexl = [i+1 for i in range(n)]
+result = []
+index = 0
+move = ballon.pop(index)
+result.append(indexl.pop(index))
+while len(ballon)>0:
+    if 0<=move:
+        index = (index + move-1)%len(ballon)
+    else:
+        index = (index + move)%len(ballon)
+    move = ballon.pop(index)
+    result.append(indexl.pop(index))
+
+for j in range(n):
+    print(result[j], end=" ")
